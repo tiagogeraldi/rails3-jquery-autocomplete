c8923a4042652f206a89cd3ac632a7ab50e1b81f
Tiago A. Geraldi <virgulla@gmail.com>
2013-11-16 12:56:40 -0200

    force activerecord test

--- lib/rails3-jquery-autocomplete/autocomplete.rb
+++ lib/rails3-jquery-autocomplete/autocomplete.rb
@@ -3,13 +3,13 @@ module Rails3JQueryAutocomplete
     def self.included(target)
       target.extend Rails3JQueryAutocomplete::Autocomplete::ClassMethods
 
-      if defined?(Mongoid::Document)
-        target.send :include, Rails3JQueryAutocomplete::Orm::Mongoid
-      elsif defined?(MongoMapper::Document)
-        target.send :include, Rails3JQueryAutocomplete::Orm::MongoMapper
-      else
+      #if defined?(Mongoid::Document)
+      #  target.send :include, Rails3JQueryAutocomplete::Orm::Mongoid
+      #elsif defined?(MongoMapper::Document)
+      #  target.send :include, Rails3JQueryAutocomplete::Orm::MongoMapper
+      #else
         target.send :include, Rails3JQueryAutocomplete::Orm::ActiveRecord
-      end
+      #end
     end
 
     #
